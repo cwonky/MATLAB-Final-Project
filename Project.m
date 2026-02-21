05f6a81b0521cf0f692d2f25501532025c2f7ca8
isabellagaeta <isabellagaeta@g.ucla.edu>
2026-02-21 12:15:34 -0800

    Update Project.m

--- Project.m
+++ Project.m
@@ -1,2 +1,6 @@
 % Solicit image from user
 image = imread(input("Enter image file name: ",'s')); 
+
+% Find boundaries between different color indices (indexing) - we also need to figure out how many different colors we want)
+boundaries = edge(indexed_img, 'sobel', 0); 
+line_art = ~boundaries; % Invert so lines are black, background is white
